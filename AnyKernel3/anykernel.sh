1ee9974e2a78c5085d5e31411d17c038780a1c87
wolverine998 <ejubcolt@gmail.com>
2023-09-25 13:27:07 -0400

    Rename some properties
    Signed-off-by: wolverine998 <ejubcolt@gmail.com>

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Spectrum Kernel by Edgars Cirulis
+kernel.string=MIUI Performance Kernel by Wolverine998
 do.devicecheck=1
 do.modules=0
 do.systemless=0
